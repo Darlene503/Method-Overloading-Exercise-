c78aff67ed5ed5e14ce8bcb93104d15f63d51372
darlene503 <duyn503@gmail.com>
2023-07-27 18:26:16 -0700

    finished overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x = 25;
+            int y = 15;
+
+            var intAdd = Add(x, y);
+            Console.WriteLine(intAdd);
+
+            var decAdd = Add(100.50m, 200m);
+            Console.WriteLine(decAdd);
+
+            var boolAdd = Add(4, 4, true);
+            Console.WriteLine(boolAdd);
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool withDollars)
+        {
+            int sum = num1 + num2;
+
+            if (withDollars && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (withDollars && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+
+            return $"{sum}";
         }
     }
 }
+
+
